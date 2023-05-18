39f08841404cac5ff87805d3fe19a53e8cd158ed
Silvia Saverino <silviasave89@gmail.com>
2023-05-18 16:34:22 +0000

    create reviews model

---
+++ reviews/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from posts.model import Post
+
+class Reviews(models.Model):
+    """
+    Reviews model, related to User and Post
+    """
+    owner =  models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    
+    class Meta:
+        ordering = ['-created_at']
+        
+    def __str__(self):
+        return self.content
