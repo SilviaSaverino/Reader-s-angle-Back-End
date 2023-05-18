b293f1c3a49b3f5e21aadfdfeca3c00280b9f945
Silvia Saverino <silviasave89@gmail.com>
2023-05-18 17:12:40 +0000

    add views list and details with crud functionalities, using generics to the Review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from posts.model import Post
+from posts.models import Post
 
 class Review(models.Model):
     """
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from readersandle_api.permissions import IsOwnerOrReadOnly
+from .models import Review
+from .serializers import ReviewSerializer, ReviewDetailSerializer
 
-# Create your views here.
+
+class ReviewList(generics.ListCreateAPIView):
+    """
+    API endpoint for listing and creating reviews.
+    """
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Review.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a review by its
+    id if the current user owns it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ReviewDetailSerializer
+    queryset = Review.objects.all()
