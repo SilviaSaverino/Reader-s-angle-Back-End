99f47dcd1ff100054ab7e716c275188952e016a3
Silvia Saverino <silviasave89@gmail.com>
2023-05-19 14:57:11 +0000

    create likelist and likedetail views to add,retrieve or destroy a like; also added urls paths

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from readersandle_api.permissions import IsOwnerOrReadOnly
+from .models import Like
+from .serializers import LikeSerializer
 
-# Create your views here.
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    API endpoint for listing and creating likes.
+    """
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve or destroy a like by its id. 
+    A user is able to like a post
+    only if is logged in.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
