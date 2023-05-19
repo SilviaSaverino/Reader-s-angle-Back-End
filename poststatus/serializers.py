75ed1fa884bafdc84a902f028f7f119cab34c1d7
Silvia Saverino <silviasave89@gmail.com>
2023-05-19 15:48:20 +0000

    add serializer to poststatus

---
+++ poststatus/serializers.py
@@ -0,0 +1,14 @@
+from django.db import IntegrityError
+from rest_framework import serializers
+from .models import PostStatus
+
+
+class PostStatusSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the PostStatus model
+    """
+    owner = serializers.ReadOnlyField(source="owner.username")
+    
+    class Meta:
+        model: PostStatus
+        fields = ['id', 'post', 'owner', 'status']
