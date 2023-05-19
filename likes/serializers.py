f5ca13598a8e7948c27c7e140723078bc5866f5b
Silvia Saverino <silviasave89@gmail.com>
2023-05-19 14:41:26 +0000

    add create  function and integrity error to like serializer to avoid duplicate likes on same post

--- likes/serializers.py
+++ likes/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from rest_framework import serializers
 from .models import Like
 
@@ -9,6 +10,20 @@ class LikeSerializer(serializers.ModelSerializer):
     owner = serializer.ReadOnlyField(source='owner.username')
     
     
-    class Mera:
+    class Meta:
         model = Like
         fields = ['id', 'post', 'owner', 'created_at']
+        
+    def create(self, validated_data):
+        """
+        Create a new instance of the serializer's model 
+        with the provided data.
+        If a duplicate post liked by the user is detected,
+        a validation error is raised.
+        """
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'details': 'post already liked by the user. Possible duplicate'
+            })
