b1ce9055354b0acfe5bf531a3fcae39bd9c47ece
Silvia Saverino <silviasave89@gmail.com>
2023-05-20 12:49:20 +0000

    add serializer to PostFollower model to handle its objects

---
+++ post_followers/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import PostFollower
+
+
+class PostFollowerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the PostFollower model.
+    'owner' represents the user who is following the post.
+    'followed_post' represents the ID of the followed post.
+    """
+    owner = serializers.ReadOnlyField(source='user.username')
+    followed_post = serializers.ReadOnlyField(source='post.id')
+
+    class Meta:
+        model = PostFollower
+        fields = ['id', 'owner', 'created_at', 'followed_post']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'Post already followed by user. possible duplicate'})
