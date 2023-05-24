61a2e707d5456a6887f597ef66decb92705997ce
Silvia Saverino <silviasave89@gmail.com>
2023-05-24 16:11:48 +0000

    refactor posts and profiles views using Generics Views

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from rest_framework import generics, permissions
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,52 +8,20 @@ from .serializers import ProfileSerializer
 from readersandle_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
-    API endpoint for listing profiles
+    List all profiles.
     Profile creation is handled by Django signals
     """
-    def get(self, request):
-        """
-        Get a list of all profiles.
-        """
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
-    """
-    API endpoint for retrieving, updating, or deleting a specific profile
-    """
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if the current user
+    is the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-    def get_object(self, pk):
-        """
-        Retrieve the profile object with the given primary key (pk)
-        """
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        """
-        Retrieve a specific profile by its primary key (pk)
-        """
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        """
-        Update a specific profile by its primary key (pk)
-        """
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
