9ab2be5f08067b62238fb190e3f2e4de9fb0de90
Silvia Saverino <silviasave89@gmail.com>
2023-05-24 14:30:48 +0000

    remove source from followed_post and display the post title using serializer method field and get function

--- post_followers/serializers.py
+++ post_followers/serializers.py
@@ -9,14 +9,18 @@ class PostFollowerSerializer(serializers.ModelSerializer):
     'followed_post' represents the ID of the followed post.
     """
     owner = serializers.PrimaryKeyRelatedField(read_only=True, source="owner.username", default=serializers.CurrentUserDefault())
-    followed_post = serializers.PrimaryKeyRelatedField(source="followed_post.title",queryset=Post.objects.all())
-    
+    followed_post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+    followed_post_title = serializers.SerializerMethodField(read_only=True)
+        
     class Meta:
         model = PostFollower
-        fields = ['id', 'owner', 'created_at', 'followed_post']
+        fields = ['id', 'owner', 'created_at', 'followed_post', 'followed_post_title']
 
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
             raise serializers.ValidationError({'detail': 'Post already followed by user. possible duplicate'})
+    
+    def get_followed_post_title(self, obj):
+        return obj.followed_post.title
