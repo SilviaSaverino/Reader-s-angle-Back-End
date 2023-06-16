a6d702a4235d4c69ce8a53f0243b18ec16ed1093
Silvia Saverino <silviasave89@gmail.com>
2023-06-16 14:28:57 +0000

    add missing slash on url path

--- reviewlikes/urls.py
+++ reviewlikes/urls.py
@@ -3,6 +3,6 @@ from reviewlikes import views
 
 urlpatterns = [
     path('reviewlikes/', views.ReviewLikeList.as_view()),
-    path('reviewlikes/<int:pk>', views.ReviewLikeDetail.as_view())
+    path('reviewlikes/<int:pk>/', views.ReviewLikeDetail.as_view())
 ]
 
